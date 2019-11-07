271a8e087c6904cfc805c90ecbad33dae131ceb2
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-11-07 20:36:42 -0300

    Adicionado método criar pauta

--- sireata-control/src/main/java/br/edu/utfpr/dv/sireata/apresentacao/PautaController.java
+++ sireata-control/src/main/java/br/edu/utfpr/dv/sireata/apresentacao/PautaController.java
@@ -9,10 +9,14 @@ import br.edu.utfpr.dv.sireata.dao.PautaDAO;
 import br.edu.utfpr.dv.sireata.model.Pauta;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,14 +28,28 @@ public class PautaController {
         this.pautaDAO = new PautaDAO();
     }
             
-    @GetMapping("/pauta/{id}")
-    public List<Pauta> listaPautas(@PathVariable(value = "id") int id) {
+    @GetMapping("/pauta/ata/{id}")
+    public List<Pauta> lista(@PathVariable(value = "id") int id) {
         try {
             Pauta pauta = new Pauta();
             pautaDAO.salvar(pauta);
-            return pautaDAO.listarPorAta(1);
+            List<Pauta> lista = pautaDAO.listarPorAta(id);
+            return lista;
         } catch(SQLException ex) {
             return null;
         }
+    }
+    
+    @PostMapping("/pauta")
+    public HttpStatus criar(Pauta pauta) {
+        
+        try {
+            pautaDAO.salvar(pauta);
+        } catch (SQLException ex) {
+            Logger.getLogger(PautaController.class.getName()).log(Level.SEVERE, null, ex);
+            return HttpStatus.CREATED;
+        }
+        
+        return HttpStatus.CREATED;
     }
 }
